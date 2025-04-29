e27253bcc4ac3d93586014e555a4d65a9040d434
Alexandre MARY <alexandre.mary@meteo.fr>
2025-04-29 15:00:07 +0000

    partially add new/updated tests from PR32 commit c3d07432

---
+++ src/tasks/assim/arome_BS3D_OOPS__obstype.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Loop on obstypes to test BatorODB+Screening independantly on each obstypes.
+"""
+
+import vortex
+from vortex import toolbox
+from vortex.layout.nodes import Driver, Family, LoopFamily
+
+from .raw2odb.batodbLAM import BatorODB
+from .screenings.screeningOOPS_LAM3D import ScreeningOOPS
+
+
+def setup(t, **kw):
+    return Driver(tag='drv', ticket=t, options=kw, nodes=[
+        Family(tag='arome', ticket=t, nodes=[
+            Family(tag='3dvar3h', ticket=t, nodes=[
+                Family(tag='default_compilation_flavour', ticket=t, nodes=[
+                    LoopFamily(tag='obstypes', ticket=t,
+                        loopconf='obstypes',
+                        loopsuffix='.{}',
+                        nodes=[
+                            BatorODB(tag='batodb', ticket=t, **kw),
+                            ScreeningOOPS(tag='screeningOOPS', ticket=t, **kw),
+                        ], **kw),
+                    ], **kw),
+                ], **kw),
+            ], **kw),
+        ],
+    )
+
---
+++ src/tasks/assim/arome_BSM3DVar_OOPS.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+import vortex
+from vortex import toolbox
+from vortex.layout.nodes import Driver, Family, LoopFamily
+
+from .raw2odb.batodbLAM import BatorODB
+from .screenings.screeningOOPS_LAM3D import ScreeningOOPS
+from .minims.minimOOPS_LAM3D import MinimNoVARBC as MinimOOPSNoVARBC
+
+
+def setup(t, **kw):
+    return Driver(tag='drv', ticket=t, options=kw, nodes=[
+        Family(tag='arome', ticket=t, nodes=[
+            Family(tag='3dvar3h', ticket=t, nodes=[
+                Family(tag='default_compilation_flavour', ticket=t, nodes=[
+                    LoopFamily(tag='rundates', ticket=t,
+                        loopconf='rundates',
+                        loopsuffix='.{}',
+                        nodes=[
+                            BatorODB(tag='batodb', ticket=t, **kw),
+                            ScreeningOOPS(tag='screeningOOPS', ticket=t, **kw),
+                            MinimOOPSNoVARBC(tag='minimOOPSNoVARBC', ticket=t, **kw),
+                        ], **kw),
+                    ], **kw),
+                ], **kw),
+            ], **kw),
+        ],
+    )
+

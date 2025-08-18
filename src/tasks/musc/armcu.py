2b70abb8aa2136a28a8d7668b7ceff02665fd9ba
Alexandre MARY <alexandre.mary@meteo.fr>
2025-08-18 08:34:09 +0000

    add musc armcu test case for arpege and arome

---
+++ src/tasks/musc/armcu.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+
+from vortex.layout.nodes import Family, Driver, LoopFamily
+
+from .musc import MUSCForecast
+
+
+def setup(t, **kw):
+    return Driver(tag='drv', ticket=t, options=kw, nodes=[
+        LoopFamily(tag='models', ticket=t,
+            loopconf='model_configs',
+            loopsuffix='.{}',
+            nodes=[
+                Family(tag='case_armcu', ticket=t, on_error='delayed_fail', nodes=[
+                    LoopFamily(tag='gmkpack', ticket=t,
+                        loopconf='compilation_flavours',
+                        loopsuffix='.{}',
+                        nodes=[
+                            MUSCForecast(tag='model', ticket=t, on_error='delayed_fail', **kw),
+                        ], **kw),
+                    ], **kw),
+                ], **kw),
+        ],
+    )

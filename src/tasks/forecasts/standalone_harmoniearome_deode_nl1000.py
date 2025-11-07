9c0a2e74caa906b2ee1943ea34f3760fc7f70a77
yogeshk12 <yogeshk@met.no>
2025-11-07 10:53:55 +0000

    harmonie_arome_deode_CY50T1_DAVAI_testing

---
+++ src/tasks/forecasts/standalone_harmoniearome_deode_nl1000.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from vortex.layout.nodes import Family, Driver, LoopFamily
+
+from .standalone.arome import StandaloneAromeForecast
+
+
+def setup(t, **kw):
+    return Driver(tag='drv', ticket=t, options=kw, nodes=[
+        LoopFamily(tag='gmkpack', ticket=t,
+            loopconf='compilation_flavours',
+            loopsuffix='.{}',
+            nodes=[
+                Family(tag='harmoniearome', ticket=t, on_error='delayed_fail', nodes=[
+                    Family(tag='nl1000', ticket=t, nodes=[
+                        StandaloneAromeForecast(tag='arome_nominal', on_error='delayed_fail', ticket=t, **kw),
+                        StandaloneAromeForecast(tag='arome_nproma32', ticket=t, **kw),
+                        ], **kw),
+                    ], **kw),
+                ], **kw),
+        ],
+    )
+
